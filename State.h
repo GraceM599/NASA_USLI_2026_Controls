0af8cdede6cd50a4883039ef4e2991849b1a009c
yim1305 <145517547+yim1305@users.noreply.github.com>
2025-12-26 16:17:41 -0500

    Create State.h

---
+++ State.h
@@ -0,0 +1,28 @@
+#ifndef STATE_H
+#define STATE_H
+
+// Current state (holds latest sensor readings)
+struct State {
+    // IMU
+    float accelX, accelY, accelZ;  // m/s²
+    float gyroX, gyroY, gyroZ;     // rad/s
+    
+    // Barometer
+    float baroAltitude;            // meters
+    float baroPressure;            // hPa
+    float baroTemperature;         // °C
+    
+    // GPS
+    float gpsLat;
+    float gpsLon;
+    float gpsAltitude;
+    float gpsSpeed;
+    uint8_t gpsSatellites;
+    bool gpsHasFix;
+    
+    // TODO: Kalman estimates
+    float estimatedAltitude;    // meters 
+    float estimatedVelocity;    // m/s
+};
+
+#endif
